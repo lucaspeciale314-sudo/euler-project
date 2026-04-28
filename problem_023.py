b8a64693acf8f78961a76b260dff6ee81dec9c14
lucaspeciale314-sudo <lucaspeciale3.14@gmail.com>
2026-04-28 10:37:34 +0200

    Update problem_023.py

--- problem_023.py
+++ problem_023.py
@@ -1,8 +1,9 @@
+# Problem 23: Sum of non-abundant numbers 
 import math
 def divisors_sum(n):
     if n==1: return 0
     total=1
-    for i in range(2,int(math.isqrt(n))+1):
+    for i in range(2,math.isqrt(n)+1):
         if n % i == 0:
             if i * i == n:
                 total += i
@@ -31,3 +32,6 @@ sum_nonabundant_numbers = sum(
 )
 
 print(sum_nonabundant_numbers)
+
+list_nonabundant_numbers=[n for n in range(1,LIMIT + 1) if n not in sum_of_two_abundant_numbers ]
+print(f" Here is the list of all non-abundant numbers: {list_nonabundant_numbers}")
