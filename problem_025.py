28bdf8544e7223c3d45118a7b7325092d9e3bef0
lucaspeciale314-sudo <lucaspeciale3.14@gmail.com>
2026-04-29 15:07:31 +0200

    Update problem_025.py

--- problem_025.py
+++ problem_025.py
@@ -1,10 +1,11 @@
 import math
 LIMIT=1000
+Phi = (1+math.sqrt(5))/2
+LOG_SQRT5 = math.log10(math.sqrt(5))
 def fibonacci_digits(m):
-    Phi=(1+math.sqrt(5))/2
     # We use log10(F_m) ~ m log10(phi) - log10(sqrt(5)),
     # since computing phi**m would become too large.
-    d_m = math.log10((Phi)) * m - math.log10(math.sqrt(5))
+    d_m = math.log10((Phi)) * m - LOG_SQRT5 
     return int(d_m)+1
 n=2
 fibonacci_dig=1
