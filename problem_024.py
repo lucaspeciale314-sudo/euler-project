adef207e3cf636095b071896a7d554500edcdff3
lucaspeciale314-sudo <lucaspeciale3.14@gmail.com>
2026-04-29 14:27:07 +0200

    Update problem_024.py

--- problem_024.py
+++ problem_024.py
@@ -1,21 +1,7 @@
-import numpy as np
-i=0
-LIMIT=1000000
-millionth_perm=0
-for k in range(1,1):
-    if k<1000000000 and len(set(str(k))) == 9 and 0 not in set(str(k)):
-        i += 1 
-        print(k)
-    if len(set(str(k))) == 10: 
-        i += 1 
-        print(k)
-    if i == LIMIT: 
-        millionth_perm=k 
-        break
-
-print(millionth_perm)
-print(set(str(1234560789)))
-
+# Problem 24: we use the fact that we can compute artificially the first digit by observing that the smallest 9! 
+# permutations will start with 0, the next 9! with 1, etc. etc. 9! = 362 880 and 9! * 2 + 274 240 = 1 000 000.
+# This is necessary to have the result in a acceptable time (~1s).
+# A better method would be to inductively use the reasoning used to choose '2' to choose the next digits as well.
 def inductive_permutation(inductive_list):
     new_inductive_list=[]
     for k in inductive_list:
@@ -23,10 +9,9 @@ def inductive_permutation(inductive_list):
             if l not in k:
                 new_inductive_list.append(k+[l])
     return new_inductive_list
-digits=[]
-for i in range(0,10): digits.append([i])
+digits=[[2]]
+#for i in range(0,10): digits.append([i])
 perm_list=digits
 for j in range(1,10): perm_list=inductive_permutation(perm_list)
-print(inductive_permutation(digits))
-    
+print(perm_list[274239])
     
